8c6e3d1e26e04ddddc0f7b80a242e353c6c7f385
DraimDEV <danilaorlov4@gmail.com>
2022-07-10 11:10:53 +0300

    [1.0.10] Удалена команда /dc stop

--- src/main/java/net/draimcido/draimcamera/Commands/CameraCmds/Stop.java
+++ src/main/java/net/draimcido/draimcamera/Commands/CameraCmds/Stop.java
@@ -2,9 +2,11 @@ package net.draimcido.draimcamera.Commands.CameraCmds;
 
 import net.draimcido.draimcamera.Commands.DraimCameraCommand;
 import net.draimcido.draimcamera.Main;
+import net.draimcido.draimcamera.Utils.Camera.CameraMode;
 import net.draimcido.draimcamera.Utils.Camera.CmdExecutor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Stop extends DraimCameraCommand {
 
@@ -14,8 +16,21 @@ public class Stop extends DraimCameraCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (sender instanceof Player) {
+            if (sender.hasPermission("draimcamera.cmd.stop")) {
+                if (this.plugin.player_camera_mode.get(((Player) sender).getUniqueId()) != null && this.plugin.player_camera_mode.get(((Player) sender).getUniqueId()) != CameraMode.NONE
+                        &&  this.plugin.player_camera_handler.get(((Player) sender).getUniqueId()) != null) {
+                this.plugin.player_camera_handler.get(((Player) sender).getUniqueId()).stop();
+                if (!sender.hasPermission("draimcamera.hidestartmessage")) sender.sendMessage(plugin.getConfig().getString("Messages.Commands.Stop.stopped"));
+                } else {
+                    sender.sendMessage(plugin.getConfig().getString("Messages.Commands.Stop.not-started"));
+                }
 
-        // TODO: Add command to stop cmds
+            } else {
+                sender.sendMessage(plugin.getConfig().getString("Messages.Commands.Stop.no-permission"));
+            }
+        }
+
         return false;
     }
 }
