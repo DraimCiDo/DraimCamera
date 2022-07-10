abf12df701bc21b00771feb1dce18814c3f0b60c
DraimDEV <danilaorlov4@gmail.com>
2022-07-10 10:28:42 +0300

    [1.0.5] Добавлена команда /dc removepoint

--- src/main/java/net/draimcido/draimcamera/Commands/CameraCmds/RemovePoint.java
+++ src/main/java/net/draimcido/draimcamera/Commands/CameraCmds/RemovePoint.java
@@ -5,6 +5,7 @@ import net.draimcido.draimcamera.Main;
 import net.draimcido.draimcamera.Utils.Camera.CmdExecutor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class RemovePoint extends DraimCameraCommand {
 
@@ -14,8 +15,29 @@ public class RemovePoint extends DraimCameraCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (sender.hasPermission("draimcamera.cmd.removepoint")) {
+            if (args.length == 0 || args.length == 1) {
+                int num = -1;
+                if (args.length == 1) {
+                    num = Integer.parseInt(args[0]) - 1;
+                }
 
-        // TODO: Add command to remove point from camera
+                String camera_name = plugin.player_selected_camera.get(((Player) sender).getUniqueId());
+                if (camera_name != null) {
+                    plugin.getConfigCameras().camera_removepoint(camera_name, num);
+                    sender.sendMessage(plugin.getConfig().getString("Messages.Commands.RemovePoint.success"));
+                } else {
+                    sender.sendMessage(plugin.getConfig().getString("Messages.Commands.RemovePoint.no-camera"));
+                    sender.sendMessage(plugin.getConfig().getString("Messages.Commands.RemovePoint.help"));
+                }
+
+            } else {
+                sender.sendMessage(plugin.getConfig().getString("Messages.Commands.RemovePoint.help"));
+            }
+
+        } else {
+            sender.sendMessage(plugin.getConfig().getString("Messages.Commands.RemovePoint.no-permission"));
+        }
         return false;
     }
 }
