e8298488b375635034b95dbfb96415a29883c093
DraimDEV <danilaorlov4@gmail.com>
2022-07-10 11:21:38 +0300

    [1.0.13] Пропись ивента на движение

--- src/main/java/net/draimcido/draimcamera/Events/OnMove.java
+++ src/main/java/net/draimcido/draimcamera/Events/OnMove.java
@@ -1,4 +1,26 @@
 package net.draimcido.draimcamera.Events;
 
-public class OnMove {
+import net.draimcido.draimcamera.Main;
+import net.draimcido.draimcamera.Utils.Camera.CameraMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class OnMove implements Listener {
+
+    private Main plugin;
+
+    public OnMove(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (plugin.player_camera_mode.get(event.getPlayer().getUniqueId()) != null) {
+            if (plugin.player_camera_mode.get(event.getPlayer().getUniqueId()) == CameraMode.PREVIEW) {
+                event.setCancelled(true);
+            }
+        }
+    }
 }
