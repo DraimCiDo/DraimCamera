9d9359a451ec0c04aaf7c523284585532b9184e7
DraimDEV <danilaorlov4@gmail.com>
2022-07-10 10:38:08 +0300

    [1.0.7] Добавлена команда /dc setduration

--- src/main/java/net/draimcido/draimcamera/Commands/CameraCmds/SetDuration.java
+++ src/main/java/net/draimcido/draimcamera/Commands/CameraCmds/SetDuration.java
@@ -2,9 +2,11 @@ package net.draimcido.draimcamera.Commands.CameraCmds;
 
 import net.draimcido.draimcamera.Commands.DraimCameraCommand;
 import net.draimcido.draimcamera.Main;
+import net.draimcido.draimcamera.Utils.Camera.CameraUtils;
 import net.draimcido.draimcamera.Utils.Camera.CmdExecutor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class SetDuration extends DraimCameraCommand {
 
@@ -14,8 +16,29 @@ public class SetDuration extends DraimCameraCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (sender.hasPermission("draimcamera.cmd.setduration")) {
+            if (args.length == 1) {
+                int duration = CameraUtils.timeStringToSecondsConvertor(args[0]);
 
-        // TODO: Add command to set duration of camera
+                if (duration > 0) {
+                    String camera_name = plugin.player_selected_camera.get(((Player) sender).getUniqueId());
+                    if (camera_name != null) {
+                        plugin.getConfigCameras().setDuration(camera_name, duration);
+                        sender.sendMessage(plugin.getConfig().getString("Messages.Commands.SetDuration.success"));
+                    } else {
+                        sender.sendMessage(plugin.getConfig().getString("Messages.Commands.Select.not-select"));
+                        sender.sendMessage(plugin.getConfig().getString("Messages.Commands.Select.help"));
+                    }
+
+                } else {
+                    sender.sendMessage(plugin.getConfig().getString("Messages.Commands.SetDuration.invalid-time"));
+                }
+            } else {
+                sender.sendMessage(plugin.getConfig().getString("Messages.Commands.SetDuration.help"));
+            }
+        } else {
+            sender.sendMessage(plugin.getConfig().getString("Messages.Commands.SetDuration.no-permission"));
+        }
         return false;
     }
 }
