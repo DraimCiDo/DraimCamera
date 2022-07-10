d3aad79be971739e737428340ca0b840531eb522
DraimDEV <danilaorlov4@gmail.com>
2022-07-10 10:32:02 +0300

    [1.0.6] Добавлена команда /dc select

--- src/main/java/net/draimcido/draimcamera/Commands/CameraCmds/Select.java
+++ src/main/java/net/draimcido/draimcamera/Commands/CameraCmds/Select.java
@@ -5,6 +5,7 @@ import net.draimcido.draimcamera.Main;
 import net.draimcido.draimcamera.Utils.Camera.CmdExecutor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Select extends DraimCameraCommand {
 
@@ -14,8 +15,21 @@ public class Select extends DraimCameraCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-
-        // TODO: Add command to select camera
+        if (sender.hasPermission("draimcamera.cmd.select")) {
+            if (args.length == 1) {
+                String camera_name = args[0];
+                if (plugin.getConfigCameras().camera_exists(camera_name)) {
+                    plugin.player_selected_camera.put(((Player) sender).getUniqueId(), plugin.getConfigCameras().get_camera_name_ignorecase(camera_name));
+                    sender.sendMessage(plugin.getConfig().getString("Messages.Commands.Select.selected"));
+                } else {
+                    sender.sendMessage(plugin.getConfig().getString("Messages.Commands.Select.not-select"));
+                }
+            } else {
+                sender.sendMessage(plugin.getConfig().getString("Messages.Commands.Select.help"));
+            }
+        } else {
+            sender.sendMessage(plugin.getConfig().getString("Messages.Commands.Select.no-permission"));
+        }
         return false;
     }
 }
